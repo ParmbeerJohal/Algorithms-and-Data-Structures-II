aab1b6bcb381003d1d993b62f21cf324b2c9ceaa
ParmbeerJohal <parmbeerjohal@gmail.com>
2020-06-07 12:26:55 -0700

    updated assignment, added test input

--- Assignment3/ShortestPaths.java
+++ Assignment3/ShortestPaths.java
@@ -1,40 +1,51 @@
 /* ShortestPaths.java
    CSC 226 - Fall 2018
-   
+
    This template includes some testing code to help verify the implementation.
    To interactively provide test inputs, run the program with
    java ShortestPaths
-   
+
    To conveniently test the algorithm with a large input, create a text file
    containing one or more test graphs (in the format described below) and run
    the program with
    java ShortestPaths file.txt
    where file.txt is replaced by the name of the text file.
-   
+
    The input consists of a series of graphs in the following format:
-   
+
    <number of vertices>
    <adjacency matrix row 1>
    ...
    <adjacency matrix row n>
-   
-   Entry A[i][j] of the adjacency matrix gives the weight of the edge from 
+
+   Entry A[i][j] of the adjacency matrix gives the weight of the edge from
    vertex i to vertex j (if A[i][j] is 0, then the edge does not exist).
    Note that since the graph is undirected, it is assumed that A[i][j]
    is always equal to A[j][i].
-   
+
    An input file can contain an unlimited number of graphs; each will be processed separately.
-   
+
    NOTE: For the purpose of marking, we consider the runtime (time complexity)
          of your implementation to be based only on the work done starting from
 	 the ShortestPaths() method. That is, do not not be concerned with the fact that
 	 the current main method reads in a file that encodes graphs via an
 	 adjacency matrix (which takes time O(n^2) for a graph of n vertices).
-   
-   
+
+
    (originally from B. Bird - 08/02/2014)
    (revised by N. Mehta - 10/24/2018)
+
+
 */
+
+/**
+ *
+ * Parm Johal
+ * November 8, 2018
+ * CSC 226 Programming Assignment 3
+ * QuickSelect.java
+ *
+ */
 
 import java.util.Scanner;
 import java.io.File;
@@ -45,35 +56,140 @@ import java.util.LinkedList;
 import java.util.Iterator;
 
 
+class Vertex {
+		public int vNum;
+		public boolean visited;
+		public int shortestDistance;
+		public boolean relaxed;
+		public ArrayList<int> path = new ArrayList<int>();
+
+
+		public Vertex(int vNum) {
+			this.vNum = vNum;
+			visited = false;
+			shortestDistance = -1;
+			relaxed = false;
+
+
+		}
+
+}
+
+class heap {
+	public int parent(int index) {
+		return (index+1)/2-1;
+	}
+	public int leftChild(int index) {
+		return (index+1)*2-1;
+	}
+	public int rightChild(int index) {
+		return (index+1)*2;
+	}
+	public boolean hasParent(int index) {
+		return index > 1;
+	}
+	public boolean hasLeftChild(int index) {
+		return leftChild(index) < size;
+	}
+	public boolean hasRightChild(int index) {
+		return rightChild(index) < size;
+	}
+	public void swap(Vertex[] array, int index1, int index2) { // used to swap 2 tasks, used for parent/child swaps in the heap array.
+		Vertex temp1 = array[index1];
+		array[index1] = array[index2];
+		array[index2] = temp1;
+	}
+
+	public void bubbleUp(int index) {
+		if(index == 0) {return;}
+		if(heap[index].compareTo(heap[parent(index)]) < 0) {
+			swap(heap, parent(index), index);
+			bubbleUp(parent(index));
+		}
+	}
+
+	public void bubbleDown(int index) {
+		if(!hasLeftChild(index)) {return;}
+		if(hasLeftChild(index) && !hasRightChild(index)) {
+			if(heap[leftChild(index)].compareTo(heap[index]) < 0) {
+				swap(heap, index, leftChild(index));
+				bubbleDown(leftChild(index));
+			} else{return;}
+		} else {
+			if(heap[leftChild(index)].compareTo(heap[rightChild(index)]) < 0) {
+				if(heap[index].compareTo(heap[leftChild(index)]) > 0) {
+					swap(heap, index, leftChild(index));
+					bubbleDown(leftChild(index));
+				} else {return;}
+			}
+			else {
+				if(heap[index].compareTo(heap[rightChild(index)]) > 0) {
+				swap(heap, index, rightChild(index));
+				bubbleDown(rightChild(index));
+				} else {return;}
+			}
+		}
+	}
+
+
+}
+
 //Do not change the name of the ShortestPaths class
 public class ShortestPaths{
 
+	static Vertex[] vertexList;
     //TODO: Your code here
     public static int n; // number of vertices
-    /* ShortestPaths(adj) 
+    /* ShortestPaths(adj)
        Given an adjacency list for an undirected, weighted graph, calculates and stores the
        shortest paths to all the vertices from the source vertex.
-       
+
        The number of vertices is adj.length
        For vertex i:
          adj[i].length is the number of edges
          adj[i][j] is an int[2] that stores the j'th edge for vertex i, where:
            the edge has endpoints i and adj[i][j][0]
            the edge weight is adj[i][j][1] and assumed to be a positive integer
-       
+
        All weights will be positive.
     */
     static void ShortestPaths(int[][][] adj, int source){
-	n = adj.length;
-	
+		/*
+		For v in V
+d[v] = ; [v] = null;
+d[s] = 0
+S =
+Q = BuildPriorityQueue(V, d)
+While Q not empty
+u = ExtractMin(Q)
+S = S u
+For v in Adj[u]
+If d[u] + w(u,v) < d[v]
+d[v] = d[u] + w(u,v)
+[v] = u
+UpdatePQ(v, [v])
+*/
+		n = adj.length;
+		vertexList = new Vertex[n];
+		for (int i = 0; i < n; i++){
+			vertexList[i] = new Vertex(i);
+		}
+		vertexList[source].shortestDistance = 0;
+		ArrayList<Vertex> path = new ArrayList<Vertex>();
+		PriorityQueue<Integer> closestV = new PriorityQueue<Integer>(n);
+		closestV.add(source);
+
 	//TODO: Your code here
     }
-    
+
     static void PrintPaths(int source){
 	//TODO: Your code here
+		for (int i = 0; i < n; i++){
+			System.out.println("The path from " + source + " to " + i + " is: " + vertexList[i].shortestDistance + " and the total distance is: " + vertices[i].path);
+		}
     }
-    
-    
+
+
     /* main()
        Contains code to test the ShortestPaths function. You may modify the
        testing code if needed, but nothing in this function will be considered
@@ -97,10 +213,10 @@ public class ShortestPaths{
 	    s = new Scanner(System.in);
 	    System.out.printf("Reading input values from stdin.\n");
 	}
-	
+
 	int graphNum = 0;
 	double totalTimeSeconds = 0;
-	
+
 	//Read graphs until EOF is encountered (or an error occurs)
 	while(true){
 	    graphNum++;
@@ -109,10 +225,10 @@ public class ShortestPaths{
 	    System.out.printf("Reading graph %d\n",graphNum);
 	    int n = s.nextInt();
 	    int[][][] adj = new int[n][][];
-	    
+
 	    int valuesRead = 0;
 	    for (int i = 0; i < n && s.hasNextInt(); i++){
-		LinkedList<int[]> edgeList = new LinkedList<int[]>(); 
+		LinkedList<int[]> edgeList = new LinkedList<int[]>();
 		for (int j = 0; j < n && s.hasNextInt(); j++){
 		    int weight = s.nextInt();
 		    if(weight > 0) {
@@ -130,7 +246,7 @@ public class ShortestPaths{
 		System.out.printf("Adjacency matrix for graph %d contains too few values.\n",graphNum);
 		break;
 	    }
-	    
+
 	    // output the adjacency list representation of the graph
 	    for(int i = 0; i < n; i++) {
 	    	System.out.print(i + ": ");
@@ -139,14 +255,14 @@ public class ShortestPaths{
 	    	}
 	    	System.out.print("\n");
 	    }
-	    
+
 	    long startTime = System.currentTimeMillis();
-	    
+
 	    ShortestPaths(adj, 0);
 	    PrintPaths(0);
 	    long endTime = System.currentTimeMillis();
 	    totalTimeSeconds += (endTime-startTime)/1000.0;
-	    
+
 	    //System.out.printf("Graph %d: Minimum weight of a 0-1 path is %d\n",graphNum,totalWeight);
 	}
 	graphNum--;
